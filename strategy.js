351849394ac2bed8e38bc3106e153535103f223f
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-07 22:32:40 +0300

    Add Strategy to make a random generated choice
    This is when I did the strategy of choosing random rock, paper or
    scissors

--- strategy.js
+++ strategy.js
@@ -1,2 +1,29 @@
-let humanChoice = prompt("What is your choice?", " ");
-console.log(humanChoice);
+// TODO: get human choice
+//
+// FIX: Apply after you finish
+// let humanChoice = prompt("What is your choice?", " ");
+// console.log(humanChoice);
+
+// TODO: generate computer choice
+
+function generateChoice() {
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerChoice;
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
+}
+console.log(generateChoice());
+// TODO: find a way to store the scores
+// TODO: show the scores
+// TODO: Write logic to play round
+// TODO: Write logic to play the entire game of 5 rounds
